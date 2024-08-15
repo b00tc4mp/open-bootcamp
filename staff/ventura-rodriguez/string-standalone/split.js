155d351f0abf7e514412ba9995381365cd7e20ff
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-08-15 16:31:37 +0200

    add split method #19

--- staff/ventura-rodriguez/string-standalone/split.js
+++ staff/ventura-rodriguez/string-standalone/split.js
@@ -25,28 +25,27 @@ function split(string, separator = 0, limit = Infinity) {
 }
 
 {
-  function arrayIsEqual(arr1, arr2) {
+  function isArrayEqual(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
+
     let result = true;
     let i = 0;
     while (i < arr1.length || result === false) {
-      if (arr1[i] !== arr2[i]) {
-        result = false;
-      }
+      if (arr1[i] !== arr2[i]) result = false;
       i++;
     }
     return result;
   }
 
   const result1 = split("Angela,26 años", ",");
-  console.assert(arrayIsEqual(result1, ["Angela", "26 años"]), {
+  console.assert(isArrayEqual(result1, ["Angela", "26 años"]), {
     result: result1,
     message: "Test 1 No pasado",
   });
 
   const result2 = split("Monday,Tuesday,Wednesday,Thursday", ",");
   console.assert(
-    arrayIsEqual(result2, ["Monday", "Tuesday", "Wednesday", "Thursday"]),
+    isArrayEqual(result2, ["Monday", "Tuesday", "Wednesday", "Thursday"]),
     {
       result: result2,
       message: "Test 2 No pasado",
@@ -54,19 +53,19 @@ function split(string, separator = 0, limit = Infinity) {
   );
 
   const result3 = split("Monday,Tuesday,Wednesday,Thursday", ",", 2);
-  console.assert(arrayIsEqual(result3, ["Monday", "Tuesday"]), {
+  console.assert(isArrayEqual(result3, ["Monday", "Tuesday"]), {
     result: result3,
     message: "Test 3 No pasado",
   });
 
   const result4 = split("1 2 3 4 5 6", " ", 3);
-  console.assert(arrayIsEqual(result4, ["1", "2", "3"]), {
+  console.assert(isArrayEqual(result4, ["1", "2", "3"]), {
     result: result4,
     message: "Test 4 No pasado",
   });
 
   const result5 = split("2024-07-20", "-", 3);
-  console.assert(arrayIsEqual(result5, ["2024", "07", "20"]), {
+  console.assert(isArrayEqual(result5, ["2024", "07", "20"]), {
     result: result5,
     message: "Test 5 No pasado",
   });
