868531a5f48283c81159e69e8ddb0893e6a31aae
bels <bels@belss-Laptop.local>
2024-08-15 15:41:32 +0200

    add string-standalone #22

--- staff/belen-palacio/string-standalone/replace.js
+++ staff/belen-palacio/string-standalone/replace.js
@@ -1,62 +1,44 @@
-// El método replace() de String values devuelve una nueva cadena con una,
-// algunas o todas las coincidencias de un patrón reemplazadas por un reemplazo. 
-// El patrón puede ser una cadena o una expresión regular, y el reemplazo 
-// puede ser una cadena o una función llamada para cada coincidencia. 
-// Si el patrón es una cadena, solo se reemplazará la primera ocurrencia. 
-// La cadena original no se modifica.
-
-// Parametros de entrada pattern, replacement, parametro de salida
-// Una nueva cadena, con una, algunas o todas las coincidencias 
-// del patrón reemplazadas por el reemplazo especificado.
-
-
 function replace(string, pattern, replacement) {
-  let result = "";  // empiezo una cadena vacía 
-  
-  for (let i = 0; i < string.length; i++) {
-      let found = true; // asumo que el patron se encontro dentro 
+  let result = "";
 
-      for (let j = 0; j < pattern.length; j++) { // recorro pattern 
-          if (string[i + j] !== pattern[j]) { // comparo pattern con mi subcadena string de i 
-              found = false; // si no hay coincidencias se marca false 
-                 }
-      }
+  let position = 0;
+  while (position < string.length) {
+    let isFound = true;
+    for (let i = 0; i < pattern.length; i++)
+      if (string[position + i] !== pattern[i]) isFound = false;
 
-          if (found) { // si hay coincidencias completas 
-               result += replacement;
-              i = i + pattern.length -1
-                          // se reemplaza por replacement 
-               } else {
-          result += string[i];  // si no se mantiene el caracter de string i 
-      }
+    if (isFound) {
+      result += replacement;
+      position = position + pattern.length;
+    } else {
+      result += string[position];
+      position++;
+    }
   }
 
-return result; // devuelvo resultado 
-
+  return result;
 }
 
-
-
 {
-const test1 = "son las 4 de la mañana";
-const result1 = replace(test1, "mañana", "tarde");
-console.assert(test1 === "son las 4 de la mañana", {
-  result: test1,
-  message: "Test 1.1 no pasado",
-});
-console.assert(result1 === test1.replace("mañana", "tarde"), {
-  result: result1,
-  message: "Test 1.2 no pasado",
-});
+  const test1 = "son las 4 de la mañana";
+  const result1 = replace(test1, "mañana", "tarde");
+  console.assert(test1 === "son las 4 de la mañana", {
+    result: test1,
+    message: "Test 1.1 no pasado",
+  });
+  console.assert(result1 === test1.replace("mañana", "tarde"), {
+    result: result1,
+    message: "Test 1.2 no pasado",
+  });
 
   const test2 = "I think Ruth's dog is cuter than your dog!";
-const result2 = replace(test2, "Ruth's", 'ferret');
-console.assert(test2 === "I think Ruth's dog is cuter than your dog!", {
-  result: test2,
-  message: "Test 2.1 no pasado",
-});
-console.assert(result2 === test2.replace("Ruth's", 'ferret'), {
-  result: result2,
-  message: "Test 2.2 no pasado",
-});
+  const result2 = replace(test2, "Ruth's", "ferret");
+  console.assert(test2 === "I think Ruth's dog is cuter than your dog!", {
+    result: test2,
+    message: "Test 2.1 no pasado",
+  });
+  console.assert(result2 === test2.replace("Ruth's", "ferret"), {
+    result: result2,
+    message: "Test 2.2 no pasado",
+  });
 }
