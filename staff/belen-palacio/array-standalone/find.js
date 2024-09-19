36592541870989deaa3c0a7f38bf37ae483332c7
bels <bels@belss-Laptop.local>
2024-09-19 14:04:44 +0200

    add array-standalone 30

---
+++ staff/belen-palacio/array-standalone/find.js
@@ -0,0 +1,54 @@
+// El método find() devuelve el valor del primer elemento 
+// del array que cumple la función de prueba proporcionada
+
+// si el metodo esta devuelve eso si no, undefined 
+
+function find(array, callback) {
+    let result = undefined;
+    
+   for (let i = 0; i < array.length; i++) {
+      if (callback(array[i], i, array)) { // si llamo a mi funcion y encuentra 
+         result = array[i];
+    }
+   }
+    
+  return result // y si no undefined 
+}
+
+{
+    
+function isArrayEqual(arr1, arr2) {
+      if (arr1.length !== arr2.length) return false;
+  
+      let result = true;
+      let i = 0;
+      while (i < arr1.length || result === false) {
+        if (arr1[i] !== arr2[i]) result = false;
+        i++;
+      }
+      return result;
+}
+
+{
+
+ const array1 = [1, 2, 3]; 
+  const fn1 = (element) => element >= 2; // 2 
+  const result1 = find(array1, fn1);
+  const nativeResult1 = array1.find(fn1);
+
+  console.assert(isArrayEqual(result1, nativeResult1), {
+    result: result1,
+    message: "Test 1 no pasado",
+  });
+
+ const array2 = [10, 50, 300]; 
+  const fn2 = (element) => element >= 2; // estoy buscando que no encuentra
+  const result2 = find(array2, fn2);
+  const nativeResult2 = array2.find(fn2);
+
+  console.assert(isArrayEqual(result2, nativeResult2), {
+    result: result2,
+    message: "Test 2 no pasado",
+  });
+    
+}}
