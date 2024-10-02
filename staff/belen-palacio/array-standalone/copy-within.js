45d07e706d6b38aca80998d87b09ece3fef8c043
bels <bels@belss-Laptop.local>
2024-10-02 13:09:39 +0200

    add array-standalone 30

--- staff/belen-palacio/array-standalone/copy-within.js
+++ staff/belen-palacio/array-standalone/copy-within.js
@@ -4,14 +4,25 @@
 // si modifica el array 
 
 function copyWithin(array, target, start, end) {
-    
+  // for
+  // vas a usar los diferentes parámetros para "rellenar el for"
+  // target es el primer elemento dónde empiezas a copiar
+  // start es la posición en dónde empiezas a copiar
+  // end es la pisición en la que paras de copiar
+
+  // tu for va a dar x vueltas que será end - start
+    const count = end - start 
+    for (let i = 0; i < count; i++) { 
+        
+  
+  return array
 }
 
 {
-    
+
 function isArrayEqual(arr1, arr2) {
       if (arr1.length !== arr2.length) return false;
-  
+
       let result = true;
       let i = 0;
       while (i < arr1.length || result === false) {
@@ -23,14 +34,20 @@ function isArrayEqual(arr1, arr2) {
 }
 
 {
-    
-const array1 = [1, 2, 3, 4, 5];
-const result1 = copyWithin(array1, 0, 3, 5); // [4, 5, 3, 4, 5] ????
-const nativeResult1 = [4, 5, 3, 4, 5]; 
 
-console.assert(isArrayEqual(result1, nativeResult1), {
+const array1 = [1, 2, 3, 4, 5]
+const nativeArray1 = [1, 2, 3, 4, 5];
+const result1 = copyWithin(array1, 0, 3, 5); // [4, 5, 3, 4, 5]
+const nativeResult1 = nativeArray1.copyWithin(0, 3, 5); 
+
+// console.assert(isArrayEqual(result1, nativeResult1), {
+//     result: result1,
+//     message: "Test 1.1 no pasado",
+// });
+
+console.assert(array1 === result1, {
     result: result1,
-    message: "Test 1 no pasado",
+    message: "Test 1.2 no pasado",
 });
 
 }
